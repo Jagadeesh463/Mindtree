858a088d6180a994519b1541ded1d43851d10fa8
unknown <mindc1july215@iiht.tech>
2022-09-23 16:06:11 +0530

    new

---
+++ EUREKA-SERVER/src/main/java/com/mindtree/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+//Eureka-server-Application
+package com.mindtree;
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@SpringBootApplication
+@EnableEurekaServer
+//Eureka-serverClass
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
